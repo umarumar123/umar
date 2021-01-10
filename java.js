19b45dbcdbe1e5be670aaf966cd99e11dc6adcfc
umar <sobitovumar.2003@gmail.com>
2021-01-10 16:09:58 +0500

    test

---
+++ java.js
@@ -0,0 +1,14 @@
+var nick  = prompt('  Ismingizni Kiriting ')
+console.log(nick);
+
+alert(' Salom ' + nick )
+
+var num = prompt(' Yoshingizni nechchida ' )
+
+alert(' yaxshi '  +  nick  +  ' Yoshingiz tog`ri keladi '  )
+
+var a = prompt(' Siz nimalarga qiziqasiz  ' + ' HTML ' + ' CSS ' + ' JAVA ' + ' BOOTSTRAP ' )
+
+alert(' Siz shu kasbga qiziqar ekansiz     '  +  a + '   tog`rimi '   +  nick )
+
+
